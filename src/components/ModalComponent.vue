89783d116e944095829d927cf903d9e73ccfdf1a
kropski <kropski@divante.pl>
2022-12-10 23:47:53 +0100

    first modal

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,68 @@
+<template>
+    <transition name="fade">
+      <div v-show="value">
+        <div
+          class="absolute bg-black opacity-70 inset-0 z-0"
+        ></div>
+        <div
+          class="w-full max-w-lg p-3 relative mx-auto my-auto rounded-xl shadow-lg bg-white"
+        >
+          <div>
+            <div class="text-center p-3 flex-auto justify-center leading-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-16 h-16 flex items-center text-blue-500 mx-auto"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              <h2 class="text-2xl font-bold py-4">Lorem ipsum dolor sit amet</h2>
+              <p class="text-md text-gray-500 px-8">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+              </p>
+            </div>
+            <div class="p-3 mt-2 text-center space-x-4 md:block">
+              <button
+                class="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-md hover:shadow-lg hover:bg-gray-100"
+              >
+                Save
+              </button>
+              <button
+                 @click="$emit('close-modal')"
+                class="mb-2 md:mb-0 bg-blue-500 border border-blue-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-md hover:shadow-lg hover:bg-blue-600"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+</template>
+
+<script>
+ export default {
+            name: 'ModalComponent',
+            props: {
+                value: {
+                    required: true
+                }
+            }
+        };
+</script>
+
+<style>
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 500ms ease-out;
+}
+</style>
---
+++ src/views/HomeModal.vue
@@ -0,0 +1,41 @@
+<template>
+    <div 
+    class="flex flex-col items-center py-20">
+        <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="toggleModal">Show Modal
+        </button>
+            <modal-component
+            v-show="modalOpen"
+            @close-modal="toggleModal">
+            </modal-component>
+    </div>
+</template>
+
+<script >
+import ModalComponent from '@/components/ModalComponent.vue'
+
+export default {
+    components: {
+        ModalComponent
+    },
+    watch:{
+    showing(value) {
+      if (value) {
+        return document.querySelector('body').classList.add('overflow-hidden');
+      }
+      document.querySelector('body').classList.remove('overflow-hidden');
+    }
+    },
+    data() {
+        return {
+            modalOpen: false
+        }
+    },
+    methods: {
+        toggleModal() {
+            this.modalOpen = !this.modalOpen
+        }
+    },
+}
+</script>
